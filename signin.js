286b4768293f72db6bef7afc87e77b856f3eec40
Fazli7 <fj67590@ubt-uni.net>
2023-12-10 16:18:01 +0100

    Update signin.js

--- signin.js
+++ signin.js
@@ -1,47 +1,46 @@
 function validation() {
+    var resultMessage = document.getElementById('result');
+    resultMessage.innerHTML = ''; // Clear previous messages
+
     var username = document.getElementById('username').value;
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
     var confirmPassword = document.getElementById('confirmPassword').value;
 
-    var resultMessage = document.getElementById('result');
-
-    // Check if any field is empty
     if (username === '' || email === '' || password === '' || confirmPassword === '') {
         resultMessage.innerHTML = 'Please fill in all fields.';
         return false;
     }
 
-    // Check if passwords match
     if (password !== confirmPassword) {
         resultMessage.innerHTML = 'Passwords do not match.';
         return false;
     }
 
-    // Regular expression patterns for validation
     var usernamePattern = /^[a-zA-Z0-9_-]{3,16}$/;
     var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     var passwordPattern = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
-    // Validate username
     if (!usernamePattern.test(username)) {
         resultMessage.innerHTML = 'Invalid username format.';
         return false;
     }
 
-    // Validate email
     if (!emailPattern.test(email)) {
         resultMessage.innerHTML = 'Invalid email format.';
         return false;
     }
 
-    // Validate password
     if (!passwordPattern.test(password)) {
-        resultMessage.innerHTML = 'Password must be at least 8 characters long and contain at least one letter and one number.';
+        resultMessage.innerHTML = 'Password must be at least 8 characters long.';
         return false;
     }
 
-    // If all validation passes
-    resultMessage.innerHTML = 'Registration Successful';
-    return true;
-}
+    resultMessage.innerHTML = 'Successfully registered. Redirecting to login page...';
+
+
+    setTimeout(function () {
+        window.location.href = 'login.html';
+    }, 2000);
+
+    return false; }
