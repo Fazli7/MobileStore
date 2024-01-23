52aced4e16e1bc57794cf6d8fd8b35bbf4d58fbd
Drenistogu <di69105@ubt-uni.net>
2024-01-24 00:44:01 +0100

    Create mobilestore.php

---
+++ mobilestore.php
@@ -0,0 +1,350 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MOBILESTORE</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <img class="logo" src="logo.png" alt="logo">
+        <nav>
+            <ul class="nav-links">
+                <li><a href="mobilestore.html"><button1 onclick="window.history.go">Store</button1></a></li>
+                <li><a href="phones.html"><button1 onclick="window.history.go">Phones</button1></a></li>
+                <li><a href="watches.html"><button1 onclick="window.history.go">Watches</button1></a></li>
+                <li><a href="accessories.html"><button1 onclick="window.history.go">Accessories</button1></a></li>
+                <li><a href="contact.html"><button1 onclick="window.history.go">Contact Us</button1></a></li>
+                <li><a class="signin" href="signin.html"><button><b>Sign in</b></button></a>
+            </ul>
+           
+        </nav>
+       
+    </header>
+    <h1 class="text"><b>All Products</b></h1>
+
+    <div class="image-row">
+        <div class="img">
+            <img src="apple1.png" alt="iphone">
+        </div>
+        <div class="title">
+            <h2>Iphone 15 Pro Max</h2>
+
+        </div>
+        <div class="price">
+            <h3>1299€</h3>
+
+        </div>
+    </div>
+
+    <div class="image-row">
+        <div class="img">
+            <img src="iphone14pro.png" alt="iphone">
+        </div>
+        <div class="title">
+            <h2>Iphone 14 Pro</h2>
+
+        </div>
+        <div class="price">
+            <h3>900€</h3>
+
+        </div>
+    </div>
+
+    <div class="image-row">
+        <div class="img">
+            <img src="iphone12pro.png" alt="iphone">
+        </div>
+        <div class="title">
+            <h2>Iphone 12 Pro</h2>
+
+        </div>
+        <div class="price">
+            <h3>680€</h3>
+
+        </div>
+    </div>
+
+    <div class="image-row">
+        <div class="img">
+            <img src="iphone11promax.png" alt="iphone">
+        </div>
+        <div class="title">
+            <h2>Iphone 11 Pro Max</h2>
+
+        </div>
+        <div class="price">
+            <h3>480€</h3>
+
+        </div>
+    </div>
+
+    <div class="image-row">
+        <div class="img">
+            <img src="iphonex.png" alt="iphone">
+        </div>
+        <div class="title">
+            <h2>Iphone X</h2>
+
+        </div>
+        <div class="price">
+            <h3>100€</h3>
+
+        </div>
+    </div>
+
+    <div class="image-row">
+        <div class="img">
+            <img src="samsungs23.png" alt="iphone">
+        </div>
+        <div class="title">
+            <h2>Samsung s23</h2>
+
+        </div>
+        <div class="price">
+            <h3>680€</h3>
+
+        </div>
+    </div>
+
+    <div class="image-row">
+        <div class="img">
+            <img src="samsunga80.png" alt="iphone">
+        </div>
+        <div class="title">
+            <h2>Samsung A80</h2>
+
+        </div>
+        <div class="price">
+            <h3>390€</h3>
+
+        </div>
+    </div>
+
+    <div class="image-row">
+        <div class="img">
+            <img src="note10.png" alt="iphone">
+        </div>
+        <div class="title">
+            <h2>Samsung Note10</h2>
+
+        </div>
+        <div class="price">
+            <h3>480€</h3>
+
+        </div>
+    </div>
+
+    <div class="image-row">
+        <div class="img">
+            <img src="note20ultra.png" alt="iphone">
+        </div>
+        <div class="title">
+            <h2>Samsung Note 20 Ultra</h2>
+
+        </div>
+        <div class="price">
+            <h3>670€</h3>
+
+        </div>
+    </div>
+
+    <div class="image-row">
+        <div class="img">
+            <img src="applewatchse.png" alt="iphone">
+        </div>
+        <div class="title">
+            <h2>Apple Watch Se</h2>
+
+        </div>
+        <div class="price">
+            <h3>240€</h3>
+
+        </div>
+    </div>
+
+    <div class="image-row">
+        <div class="img">
+            <img src="applewatchseries7.png" alt="iphone">
+        </div>
+        <div class="title">
+            <h2>Apple Watch series 7</h2>
+
+        </div>
+        <div class="price">
+            <h3>480€</h3>
+
+        </div>
+    </div>
+
+    <div class="image-row">
+        <div class="img">
+            <img src="galaxywatch7.png" alt="iphone">
+        </div>
+        <div class="title">
+            <h2>Samsung Galaxy Watch 7</h2>
+
+        </div>
+        <div class="price">
+            <h3>380€</h3>
+
+        </div>
+    </div>
+
+    <div class="image-row">
+        <div class="img">
+            <img src="galaxywatch4.png" alt="iphone">
+        </div>
+        <div class="title">
+            <h2>Samsung Galaxy Watch 4</h2>
+
+        </div>
+        <div class="price">
+            <h3>200€</h3>
+
+        </div>
+    </div>
+
+    <div class="image-row">
+        <div class="img">
+            <img src="applecable.png" alt="iphone">
+        </div>
+        <div class="title">
+            <h2>iPHONE Charger </h2>
+
+        </div>
+        <div class="price">
+            <h3>12€</h3>
+
+        </div>
+    </div>
+
+    <div class="image-row">
+        <div class="img">
+            <img src="applewatchcharger.png" alt="iphone">
+        </div>
+        <div class="title">
+            <h2>Apple Watch Charger</h2>
+
+        </div>
+        <div class="price">
+            <h3>38€</h3>
+
+        </div>
+    </div>
+    
+    <div class="image-row">
+        <div class="img">
+            <img src="galaxywatchcharger.png" alt="iphone">
+        </div>
+        <div class="title">
+            <h2>Samsung Galaxy Watch charger </h2>
+
+        </div>
+        <div class="price">
+            <h3>480€</h3>
+
+        </div>
+    </div>
+
+    <div class="image-row">
+        <div class="img">
+            <img src="wirelescharger.png" alt="iphone">
+        </div>
+        <div class="title">
+            <h2>Wireless Charger</h2>
+
+        </div>
+        <div class="price">
+            <h3>22€</h3>
+
+        </div>
+    </div>
+
+    <div class="image-row">
+        <div class="img">
+            <img src="magsafe.png" alt="iphone">
+        </div>
+        <div class="title">
+            <h2>Magsafe</h2>
+
+        </div>
+        <div class="price">
+            <h3>48€</h3>
+
+        </div>
+    </div>
+
+    <div class="image-row">
+        <div class="img">
+            <img src="powerbank.png" alt="iphone">
+        </div>
+        <div class="title">
+            <h2>MagSafe </h2>
+
+        </div>
+        <div class="price">
+            <h3>78€</h3>
+
+        </div>
+    </div>
+
+    <div class="image-row">
+        <div class="img">
+            <img src="airpods1.png" alt="iphone">
+        </div>
+        <div class="title">
+            <h2>AirPods 1</h2>
+
+        </div>
+        <div class="price">
+            <h3>120€</h3>
+
+        </div>
+    </div>
+
+    <div class="image-row">
+        <div class="img">
+            <img src="airpods3.png" alt="iphone">
+        </div>
+        <div class="title">
+            <h2>AirPods 3</h2>
+
+        </div>
+        <div class="price">
+            <h3>220€</h3>
+
+        </div>
+    </div>
+
+    <div class="image-row">
+        <div class="img">
+            <img src="airpodspro.png" alt="iphone">
+        </div>
+        <div class="title">
+            <h2>AirPods Pro</h2>
+
+        </div>
+        <div class="price">
+            <h3>360€</h3>
+
+        </div>
+    </div>
+
+    <div class="image-row">
+        <div class="img">
+            <img src="galaxybuds.png" alt="iphone">
+        </div>
+        <div class="title">
+            <h2>Galaxy Buds</h2>
+
+        </div>
+        <div class="price">
+            <h3>80€</h3>
+
+        </div>
+    </div>
+    
+</body>
+</html>
