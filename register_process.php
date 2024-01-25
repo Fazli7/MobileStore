d2e0aca405dfc767c99f61deef3a8733287e4171
Fazli7 <fj67590@ubt-uni.net>
2024-01-25 19:27:08 +0100

    Update register_process.php

--- register_process.php
+++ register_process.php
@@ -2,18 +2,18 @@
 session_start();
 require 'config.php';
 
-if (isset($_POST['register'])) {
+if (isset($_POST['submit'])) {
     $username = $_POST['username'];
     $email = $_POST['email'];
     $password = $_POST['password'];
-    $confirm_password = $_POST['confirm_password'];
+    $confirm_password = $_POST['confirmPassword'];
 
     if (empty($username) || empty($email) || empty($password) || empty($confirm_password)) {
         $_SESSION['error'] = "All fields are required!";
-        header('location: register.php');
+        header('location: signin.php');
     } elseif ($password !== $confirm_password) {
         $_SESSION['error'] = "Passwords do not match!";
-        header('location: register.php');
+        header('location: signin.php');
     } else {
         $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
@@ -22,13 +22,13 @@ if (isset($_POST['register'])) {
 
         if (!$stmt) {
             $_SESSION['error'] = "SQL error!";
-            header('location: register.php');
+            header('location: signin.php');
         } else {
             $stmt->bind_param('ssss', $username, $email, $hashed_password, $role);
             $role = 'user';
             $stmt->execute();
             $_SESSION['success'] = "Registration successful!";
-            header('location: index.php');
+            header('location: mobilestore.php');
         }
     }
 }
